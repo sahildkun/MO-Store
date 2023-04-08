758ccbc100566628c6c3df8250bacf9afa7fe7ce
sahildkun <sahilsahoo23@gmail.com>
2023-04-08 22:54:53 +0530

    login ui

--- src/routes/sign-in/User-sign-up.jsx
+++ src/routes/sign-in/User-sign-up.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
-
+import { NavLink } from 'react-router-dom';
+import google from '../../images/Google__G__Logo.svg.png'
+import img from '../../images/pexels-photo-322207.jpeg'
 export default function UserSignUp() {
   const [userName , setUserName] = useState('');
   const [userEmail , setUserEmail] = useState('');
@@ -61,51 +63,69 @@ export default function UserSignUp() {
   }
   
   return (
-   <>
-   <div className=' bg-white my-[10vh] shadow-2xl mx-auto max-w-sm  p-10 text-black rounded-lg '>
-    <h1 className=' text-center text-3xl mb-10' id='rel'>SIGN IN</h1>
+   <div className='grid grid-cols-2'>
+   <div className=' bg-black  shadow-2xl  mx-5 p-10 text-white rounded-lg '>
+    <h1 className=' text-center text-5xl mb-2 ' >Welcome back</h1>
+    <h1 className=' text-center text-xl mb-10 font-extralight' >Please enter your contact details to connect.</h1>
     <form action="" className='flex flex-col space-y-6 justify-center' onSubmit={submitHandler}>
         
-        <div className='flex flex-col space-y-2 '>
-         <label htmlFor="name" className='text-xl font-semibold' id='company'>Name</label>
-         <input 
-         type="text" 
-         placeholder='Please enter your name' 
-         className={`w-auto border-2  p-2 rounded-md ${!nameInputInValid ? 'border-slate-500 bg-white' : 'border-red-700 '}`}
-         
-         onChange={addUsernameHandler}
-         onBlur={nameInputBlurhandler}
-         value={userName}  
-         />
-         {nameInputInValid  &&  <p className='text-sm text-red-600 '>Name is invalid*</p>}
-         </div>
-        
-        <div className='flex flex-col space-y-2'>
-         <label htmlFor="email" className='text-xl font-semibold' id='company'>Email</label>
+        <div className='flex flex-col space-y-2 mx-10 '>
+          
+         <label htmlFor="name" className='text-xl font-semibold' id='company'>Email address</label>
          <input 
          type="email" 
          placeholder='Please enter your email address' 
          name='email' 
-         className={`w-auto border-2  p-2 rounded-md ${!emailInputInValid ? 'border-slate-500 bg-white' : 'border-red-700 '}`}
+         className={`w-auto border-2 text-white bg-gray-800  p-2 rounded-md ${!emailInputInValid ? 'border-slate-500 bg-white' : 'border-red-700 '}`}
          
          onChange={addEmailhandler}
          onBlur={emailInputBlurhandler}
          value={userEmail}
          />
          {emailInputInValid  &&  <p className='text-sm text-red-600 '>Email is invalid*</p>}
+    
+         </div>
+        
+        <div className='flex flex-col mx-10 space-y-2'>
+         <label htmlFor="password" className='text-xl font-semibold' id='company'>Password</label>
+         <input 
+         type="password" 
+         placeholder='Please enter your name' 
+         className={`w-auto border-2   text-white bg-gray-800   p-2 rounded-md ${!nameInputInValid ? 'border-slate-500 bg-white' : 'border-red-700 '}`}
+         
+         onChange={addUsernameHandler}
+         onBlur={nameInputBlurhandler}
+         value={userName}  
+         />
+         {nameInputInValid  &&  <p className='text-sm text-red-600 '>Password is invalid*</p>}
          </div>
 
-         <div className=' mx-auto'>
+         <div className='flex flex-col space-y-5 mx-auto'>
          <button 
          type='submit' 
          
-         className='text-white font-semibold disabled:bg-gray-400 border-2 border-slate-900 py-2 px-3 rounded-full w-[250px]  bg-slate-900 hover:text-white cursor-pointer disabled:cursor-not-allowed  disabled:text-black' 
+         className='text-white font-semibold  disabled:bg-gray-400 border-2 w-full border-slate-900 py-2 px-3 rounded-full  bg-slate-900 hover:text-white cursor-pointer disabled:cursor-not-allowed  disabled:text-black' 
          id='rel' 
-         >SIGN IN </button>
+         >LOG IN </button>
+          <button 
+         type='button'
+         
+         className='text-white font-semibold flex flex-row items-center justify-center space-x-5  disabled:bg-gray-400 border-2 border-slate-900 py-2 px-3 rounded-full  bg-slate-900 hover:text-white cursor-pointer disabled:cursor-not-allowed  disabled:text-black' 
+         id='rel' 
+         >
+          <img src={google} alt="" className='h-5 w-5'/>
+          <p>Log in with Google</p> 
+          </button>
+         <p>Don’t have an account ? <NavLink to={'/sign-up'} className={'underline'}>Sign up here</NavLink></p>
          </div>
     </form>
    </div>
+
+   <div className=''>
+
+    <img src={img} alt="fd" className=' h-[80vh] px-5 rounded-[3rem] object-fill ' />
+   </div>
    
-   </>
+   </div>
   )
 }
