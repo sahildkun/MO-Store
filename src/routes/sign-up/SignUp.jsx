3ba14e8284de427561d777b702d1a9905bde6d88
sahildkun <sahilsahoo23@gmail.com>
2023-04-22 12:50:13 +0530

    nF error

--- src/routes/sign-up/SignUp.jsx
+++ src/routes/sign-up/SignUp.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 import { signUpSchema } from '../../schemas'
 const defaultValues = {
   displayName :  '',
-  email :  '',
+  email :  '', 
   password :  '',
   confirmPassword :  '',
 }
